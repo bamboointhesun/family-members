bfc574ee2ac013b2048e9b8910f9fde0decac9f1
limingyang <lmy@lmy.com>
2022-01-12 12:29:33 +0800

    主分支项目初始化

---
+++ src/main/java/vip/leemy/sunny/familymembers/FamilyMembersApplication.java
@@ -0,0 +1,13 @@
+package vip.leemy.sunny.familymembers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyMembersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyMembersApplication.class, args);
+    }
+
+}
